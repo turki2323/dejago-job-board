d989ff85c3b7ab5ff0c9104671edcf7072faeab9
Mak <mak@Maks-MacBook-Pro.local>
2021-04-05 15:33:29 +0300

    add job views& url

--- job/models.py
+++ job/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 JOB_TYPE = (
 ('Full Time', 'Full Time'),
-    ('Part Time ', 'Part Time'),
+('Part Time ', 'Part Time'),
 )
 
 class Job (models.Model):
@@ -16,7 +16,14 @@ class Job (models.Model):
     vacancy = models.IntegerField(default=1)
     salary = models.IntegerField(default=0)
     experience = models.IntegerField(default=1)
+    category = models.ForeignKey('Category' , on_delete=models.CASCADE)
 
 
     def __str__ (self):
         return self.title
+
+class Category(models.Model):
+    name = models.CharField(max_length=25)
+
+    def ــstr__ (self):
+        return self.name 
